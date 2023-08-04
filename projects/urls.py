3aebbabb5fc24fc4c21e8dbcf926f3205c63810a
Abhinand S S <abhinand@yougotagift.com>
2023-08-04 16:05:38 +0530

    projects.html and parts_.html are added inside project app and made changes in views and settings accordingly

--- projects/urls.py
+++ projects/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('projects/', views.projects, name="projects"),
+    path('', views.projects, name="projects"),
     path('parts_/<str:pk>/', views.parts_, name="parts"),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+projectsList = [
+    {'id': '1',
+     'movie': 'Inception',
+     'nominee': 'Di Caprio'
+     },
+    {'id': '2',
+     'movie': 'Revenent',
+     'nominee': 'Di Caprio',
+     },
+    {
+        'id': '3',
+        'movie': 'Avengers',
+        'nominee': 'RDJ',
+    },
+]
+
 
 def projects(request):
-    return render(request,'projects.html')
+    page = 'project'
+    number = 12
+    context = {'portion': page, 'num': number, 'projects': projectsList}
+    return render(request, 'projects/projects.html', context)
 
 
 def parts_(request, pk):
-    return render(request,'parts_.html')
+    projectObj = None
+    for i in projectsList:
+        if i['id'] == pk:
+            projectObj = i
+    return render(request, 'projects/parts_.html', {'project': projectObj})
