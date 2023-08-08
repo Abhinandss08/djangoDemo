5808e8fae75aebc49d700d4ebb1e41e50cbfb9b6
Abhinand S S <abhinand@yougotagift.com>
2023-08-08 16:47:40 +0530

    Added main.html and navbar.html.Template inheritance is accomplished as well as rendering pages.Created admin and fulfilled migrations.

--- projects/views.py
+++ projects/views.py
@@ -1,33 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-projectsList = [
-    {'id': '1',
-     'movie': 'Inception',
-     'nominee': 'Di Caprio'
-     },
-    {'id': '2',
-     'movie': 'Revenent',
-     'nominee': 'Di Caprio',
-     },
-    {
-        'id': '3',
-        'movie': 'Avengers',
-        'nominee': 'RDJ',
-    },
-]
+from .models import Project
 
 
-def projects(request):
-    page = 'project'
-    number = 12
-    context = {'portion': page, 'num': number, 'projects': projectsList}
-    return render(request, 'projects/projects.html', context)
+def projects(reque):
+    projects = Project.objects.all()
+    context = {'projects': projects}
+    return render(reque, 'projects/projects.html', context)
 
 
 def parts_(request, pk):
-    projectObj = None
-    for i in projectsList:
-        if i['id'] == pk:
-            projectObj = i
-    return render(request, 'projects/parts_.html', {'project': projectObj})
+    projectObj = Project.objects.get(id=pk)
+    return render(request, 'projects/parts_.html', {'projects': projectObj})
