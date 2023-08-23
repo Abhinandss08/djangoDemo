65156165dee9a2b1000e5d25cbded56afb550015
Abhinand S S <abhinand@yougotagift.com>
2023-08-23 16:23:24 +0530

    Signal actions are performed by creating a file signals.py and made changes to apps.py accordingly.

--- users/models.py
+++ users/models.py
@@ -24,7 +24,7 @@ class Profile(models.Model):
                           primary_key=True, editable=False)
 
     def __str__(self):
-        return self.user.username
+        return self.username
 
 
 class Skill(models.Model):
@@ -38,4 +38,3 @@ class Skill(models.Model):
 
     def __str__(self):
         return self.name
-
