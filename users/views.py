0b9f3693903aaa269a64e3f77e8510d597e34d46
Abhinand S S <abhinand@yougotagift.com>
2023-08-25 14:22:35 +0530

    Login and logout functionality included by the addition of a login register form and flash messsage also incorporated

--- users/views.py
+++ users/views.py
@@ -1,5 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
 from .models import Profile
+from django.contrib.auth.models import User
+
+
+def loginUser(request):
+    # Checks whether user is already authenticated or not
+    # and redirects to profile accordingly
+    if request.user.is_authenticated:
+        return redirect('profiles')
+    if request.method == 'POST':
+        # Taking username and password values if method is post
+        username = request.POST['username']
+        password = request.POST['password']
+        try:
+            # Verifying username present in database
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'Username does not exist')
+        # Either returns user object if data matches or returns None if fails
+        user = authenticate(request, username=username, password=password)
+        # If user is found creates a session and redirects to homepage
+        if user is not None:
+            login(request, user)
+            return redirect('profiles')
+        else:
+            messages.error(request, 'Username or Password is incorrect !')
+    return render(request, 'users/login_register.html')
+
+
+def logoutUser(request):
+    logout(request)
+    messages.error(request, 'User was logged out!')
+    return redirect('login')
 
 
 def profiles(request):
